f412d5a8d416d0fb9e657f9f7bde427b73e26a57
tremblinglight <melissadavidson@live.ca>
2015-09-19 20:44:40 -0400

    Added player, scripts for mouse movement.
    WOOOOOOOAAAHHH so much code

---
+++ pacifistShooter/Assets/Code/mouseLook.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseLook : MonoBehaviour {
+
+	public enum RotationAxes {
+		MouseXAndY = 0,
+		MouseX = 1,
+		MouseY = 2
+	}
+
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+
+	public float sensitivityHor = 9.0f;
+	public float sensitivityVert = 9.0f;
+
+	public float minimumVert = -45.0f;
+	public float maximumVert = 45.0f;
+
+	private float _rotationx = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseX) {
+			//horizontal rotation here
+			//transform.Rotate(0, sensitivityHor, 0);
+			//above line is old code, endless spin in one direction.
+			transform.Rotate (0, Input.GetAxis ("Mouse X") * sensitivityHor, 0);
+		} else if (axes == RotationAxes.MouseY) {
+			//vertical rotation here
+			_rotationx -= Input.GetAxis("Mouse Y") * sensitivityVert;
+			_rotationx = Mathf.Clamp(_rotationx, minimumVert, maximumVert);
+
+			float rotationY = transform.localEulerAngles.y;
+
+			transform.localEulerAngles = new Vector3(_rotationx, rotationY, 0);
+		}
+		else {
+			//both horizontal and vertical rotation here
+		}
+	}
+}
---
+++ pacifistShooter/Assets/Code/objSpin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objSpin : MonoBehaviour {
+	
+	public float speed; //Public variable for the speed of rotation.
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, speed, 0, Space.World); //make the rotate run every frame.
+	}
+}
