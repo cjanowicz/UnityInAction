fe6968bfebd3df56023395d03c98ac04891fce51
tremblinglight <melissadavidson@live.ca>
2015-09-28 19:49:31 -0400

    Added UI data.

--- pacifistShooter/Assets/Code/rayShooter.cs
+++ pacifistShooter/Assets/Code/rayShooter.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class rayShooter : MonoBehaviour {
 
@@ -8,10 +9,10 @@ public class rayShooter : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		_camera = GetComponent<Camera> ();
-
+		/*
 		Cursor.lockState = CursorLockMode.Locked;
-		Cursor.visible = false;
-	}
+		Cursor.visible = false; */
+	} 
 
 	void OnGUI() {
 		int size = 12;
@@ -22,9 +23,15 @@ public class rayShooter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0))
-		{
-			Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0);
+
+			if (Input.GetMouseButtonDown(0) &&
+			    !EventSystem.current.IsPointerOverGameObject()) {
+				Vector3 point = new Vector3(
+					GetComponent<Camera>().pixelWidth/2, GetComponent<Camera>().pixelHeight/2, 0);
+			}
+				if (Input.GetMouseButtonDown (0))
+				{
+		Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0);
 			Ray ray = _camera.ScreenPointToRay (point);
 			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit)) {
@@ -33,7 +40,7 @@ public class rayShooter : MonoBehaviour {
 				reactiveTarget target = hitObject.GetComponent<reactiveTarget>();
 				if (target != null) {
 					Debug.Log("Target hit");
-					target.ReactToHit();
+					target.ReactToHit(); 
 				} else {
 				StartCoroutine(SphereIndicator(hit.point));
 				}
