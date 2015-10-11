e5b126d92d61c23d7225f0309d3298cd094b0ed2
cjanowicz <cjanowicz@gmail.com>
2015-10-11 14:36:05 -0400

    Scoop Works  now

--- VR-Dog-Project/Assets/DogProjectAssets/Scripts/DogTongueScript.cs
+++ VR-Dog-Project/Assets/DogProjectAssets/Scripts/DogTongueScript.cs
@@ -5,9 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 public class DogTongueScript : MonoBehaviour {
 
     private Animator tongueAnim;
+    private Transform cameraTransform;
 
 	// Use this for initialization
 	void Start () {
+        cameraTransform = GameObject.Find("MainCamera").transform;
         tongueAnim = GameObject.Find("Tongue").GetComponent<Animator>();
     }
 	
@@ -15,6 +17,11 @@ public class DogTongueScript : MonoBehaviour {
 	void Update () {
         if (CrossPlatformInputManager.GetButtonDown("Fire1")) {
             tongueAnim.SetTrigger("LickTrigger");
+            RaycastHit hit;
+            if (Physics.Raycast(cameraTransform.position, cameraTransform.forward,out hit,2f)) {
+                hit.transform.SendMessage("GetLicked", SendMessageOptions.DontRequireReceiver);
+            }
         }
+
 	}
 }
---
+++ VR-Dog-Project/Assets/DogProjectAssets/Scripts/IceCreamScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceCreamScript : MonoBehaviour {
+
+    public Transform[] scoops = new Transform[3];
+    private float[] scoopSize = new float[3];
+    public int licksPerScoop = 3;
+    private int totalLicksLeft;
+
+    // Use this for initialization
+    void Awake() {
+        totalLicksLeft = licksPerScoop * scoops.Length;
+        for (int i = 0; i < scoops.Length; i++) {
+            scoopSize[i] = scoops[i].localScale.y;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void GetLicked() {
+        if (totalLicksLeft > 0) {
+            int index = Mathf.CeilToInt((float)totalLicksLeft / (float)licksPerScoop) -1;
+            scoops[index].localScale = new Vector3(scoops[index].localScale.x - scoopSize[index]/licksPerScoop,
+                scoops[index].localScale.y - scoopSize[index] / licksPerScoop, scoops[index].localScale.z - scoopSize[index] / licksPerScoop);
+            
+            totalLicksLeft--;
+        }
+    }
+}
