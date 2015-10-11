662917da8530eecca914e29d437f6201ab93f9d9
cjanowicz <cjanowicz@gmail.com>
2015-10-11 00:20:46 -0400

    Moved a prefab folder, added lick Controls
    Dog tongue now plays lick animation, which can be edited later, and I
    have moved the game prefabs.

--- VR-Dog-Project/Assets/DogProjectAssets/Scripts/DogTongueScript.cs
+++ VR-Dog-Project/Assets/DogProjectAssets/Scripts/DogTongueScript.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class DogTongueScript : MonoBehaviour {
 
+    private Animator tongueAnim;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        tongueAnim = GameObject.Find("Tongue").GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (CrossPlatformInputManager.GetButtonDown("Fire1")) {
+            tongueAnim.SetTrigger("LickTrigger");
+        }
 	}
 }
